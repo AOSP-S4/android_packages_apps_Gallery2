55a0627a8a7fe17bf4a4658695293fe11a32d133
Kerong Sui <ksui@codeaurora.org>
2013-08-14 10:15:27 +0800

    Gallery2: camera: disable all camera controls ASAP when swtich the camera
    Camera switch button is a pieItem, there is a fadeInOut animation for these
    kinds of menus. And the onClick event deliver to upper layer happens after
    the end of the animation which lasts 600ms. We should add another event
    callback before the animation to disalbe all camera controls as soon as
    possible.
    
    Change-Id: Iddcb9e9452aa0ff6ea8485c08c9c56b067995fb5

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2069,6 +2069,10 @@ public class PhotoModule
         }
     }
 
+    @Override
+    public void onCameraPickerSuperClicked() {
+    }
+
     // Preview texture has been copied. Now camera can be released and the
     // animation can be started.
     @Override
