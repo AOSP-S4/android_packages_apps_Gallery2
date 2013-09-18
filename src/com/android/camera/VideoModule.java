58d5095b78fcb296777dcc005e08ab8628ff1bb7
Kerong Sui <ksui@codeaurora.org>
2013-09-18 15:51:15 +0800

    Camera: Do not create start preview thread if one thread is alive
    It will have exception if two same thread tried to manipulate
    same UI component, check the thread status before create a new
    one.
    
    Change-Id: I6f9a5c03f46360d15fdcebcd52c48b924f5b42ad
    CRs-fixed: 545574

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -793,7 +793,7 @@ public class VideoModule implements CameraModule,
 
         showVideoSnapshotUI(false);
 
-        if (!mPreviewing) {
+        if (!mPreviewing && mStartPreviewThread == null) {
             resetEffect();
             openCamera();
             if (mActivity.mOpenCameraFail) {
