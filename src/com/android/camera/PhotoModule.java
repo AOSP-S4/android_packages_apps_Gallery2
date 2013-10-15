c76c71567e94ffc35793408c0ed232ce20bc193d
Steve Kondik <shade@chemlab.org>
2013-10-14 21:18:56 -0700

    camera: Set extra ZSL parameter
    Change-Id: Idca1814659575368c69bd9706239e1c2999e6c13

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1884,6 +1884,7 @@ public class PhotoModule
                 mCameraDevice.sendMagicSamsungZSLCommand();
             }
             // Switch on ZSL mode
+            mParameters.set("zsl", "on");
             mParameters.set("camera-mode", "1");
         }
 
